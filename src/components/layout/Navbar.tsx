e146e11d8ffe5187cfcd047b178fb4df01eeaa35
Perezcortes <perezcortes05prz@gmail.com>
2026-02-17 01:17:16 -0600

    Barra de navegacion

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useRef, useEffect } from 'react';
 import { useTheme } from '@/context/ThemeContext';
-import { useAuth } from '@/context/AuthContext'; // Importamos el contexto real
+import { useAuth } from '@/context/AuthContext'; 
 import { 
   ShoppingCart, 
   Menu, 
